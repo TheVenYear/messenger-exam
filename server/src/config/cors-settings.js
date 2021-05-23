3abb8e35afb14b1ea365746ba10c13845332e22b
TheVenYear <TheVenYear@gmail.com>
2021-05-23 18:41:33 +0300

    cors

--- server/src/config/cors-settings.js
+++ server/src/config/cors-settings.js
@@ -1,8 +1,10 @@
+import config from '.';
+
 const corsSettings = {
-  origin: [
-    'http://localhost:3000',
-    'https://thevenyear.github.io/messenger-exam/',
-  ],
+  origin:
+    config.NODE_ENV === 'production'
+      ? 'https://thevenyear.github.io/messenger-exam/'
+      : 'http://localhost:3000',
   credentials: true,
 };
 
